e27bd10a90c2e4597eba6a777d0731e279d21926
funobu <fukids34@gmail.com>
2022-11-05 17:30:01 +0900

    MP-124 Add Domain model fields of post

--- internal/domains/model/post.go
+++ internal/domains/model/post.go
@@ -1,5 +1,12 @@
 package model
 
+import "time"
+
+// Post ユーザの投稿
 type Post struct {
-	ID string
+	ID        string
+	Author    string
+	Text      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }

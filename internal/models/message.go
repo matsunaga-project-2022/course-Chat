7e5b26a48cc32bae8bda80762d943672a71baa66
funobu <fukids34@gmail.com>
2022-11-12 16:58:23 +0900

    MP-131 update: define IMessageEventer and generate imp

--- internal/models/message.go
+++ internal/models/message.go
@@ -1,10 +1,36 @@
 package models
 
-import "time"
+import (
+	"github.com/go-redis/redis/v9"
+	"time"
+)
 
 type Message struct {
 	ID        string    `db:"id"`
 	UserID    string    `db:"user_id"`
 	Text      string    `db:"text"`
 	CreatedAt time.Time `db:"created_at"`
+}
+
+type IMessageEventer interface {
+	Get(id string) (Message, error)
+	Save(message Message) error
+}
+
+type MessageEventer struct {
+	manager *redis.Client
+}
+
+func NewMessageEventer(manager *redis.Client) *MessageEventer {
+	return &MessageEventer{manager: manager}
+}
+
+func (m MessageEventer) Get(id string) (Message, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (m MessageEventer) Save(message Message) error {
+	//TODO implement me
+	panic("implement me")
 }

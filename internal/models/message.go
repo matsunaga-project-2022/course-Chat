255fb6b58fce11e322c0e4339cf8c40fd8808d9e
funobu <fukids34@gmail.com>
2022-11-12 18:47:43 +0900

    MP-131 move: MessageEventer -> MessagePublisher usecase

--- internal/models/message.go
+++ internal/models/message.go
@@ -1,10 +1,6 @@
 package models
 
 import (
-	"context"
-	"encoding/json"
-	"github.com/go-redis/redis/v9"
-	"sync"
 	"time"
 )
 
@@ -14,44 +10,4 @@ type Message struct {
 	UserID    string    `db:"user_id"`
 	Text      string    `db:"text"`
 	CreatedAt time.Time `db:"created_at"`
-}
-
-// IMessageEventer is the interface of manage publish and subscribe
-type IMessageEventer interface {
-	Subscribe(ctx context.Context, subID string, messages chan<- Message, errors chan<- error)
-	Publish(ctx context.Context, message Message) error
-}
-
-// MessageEventer is the implementation of IMessageEventer by using Redis Client
-type MessageEventer struct {
-	manager *redis.Client
-	mutex   sync.Mutex
-}
-
-// NewMessageEventer is the constructor that generate new MessageEventer instance
-func NewMessageEventer(manager *redis.Client) *MessageEventer {
-	return &MessageEventer{manager: manager}
-}
-
-// Subscribe is the method of MessageEventer that subscribe message
-func (m *MessageEventer) Subscribe(ctx context.Context, subID string, messages chan<- Message, errors chan<- error) {
-	subscribe := m.manager.Subscribe(ctx, subID)
-
-	ch := subscribe.Channel()
-	for subscription := range ch {
-		var message Message
-		if err := json.Unmarshal([]byte(subscription.Payload), &message); err != nil {
-			errors <- err
-		}
-
-		m.mutex.Lock()
-		messages <- message
-		m.mutex.Unlock()
-	}
-}
-
-// Publish is the method of MessageEventer that publish message
-func (m *MessageEventer) Publish(message Message) error {
-	//TODO implement me
-	panic("implement me")
 }
---
+++ internal/usecases/publisher.go
@@ -0,0 +1,42 @@
+package usecases
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/go-redis/redis/v9"
+	"github.com/matsunaga-project-2022/course/chat/internal/models"
+	"sync"
+)
+
+// IMessagePublisher is the interface of manage publish
+type IMessagePublisher interface {
+	Publish(ctx context.Context, subID string, messages chan<- models.Message, errors chan<- error)
+}
+
+// MessagePublisher is the implementation of IMessagePublisher by using Redis Client
+type MessagePublisher struct {
+	manager *redis.Client
+	mutex   sync.Mutex
+}
+
+// NewMessagePublisher is the constructor that generate new MessagePublisher instance
+func NewMessagePublisher(manager *redis.Client) *MessagePublisher {
+	return &MessagePublisher{manager: manager}
+}
+
+// Publish is the method of MessagePublisher that subscribe message
+func (m *MessagePublisher) Publish(ctx context.Context, subID string, messages chan<- models.Message, errors chan<- error) {
+	subscribe := m.manager.Subscribe(ctx, subID)
+
+	ch := subscribe.Channel()
+	for subscription := range ch {
+		var message models.Message
+		if err := json.Unmarshal([]byte(subscription.Payload), &message); err != nil {
+			errors <- err
+		}
+
+		m.mutex.Lock()
+		messages <- message
+		m.mutex.Unlock()
+	}
+}

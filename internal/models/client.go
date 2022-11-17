79b85a376fc6db8604f03bebf7b53ef4bbb4de71
funobu <fukids34@gmail.com>
2022-11-18 01:46:44 +0900

    MP-144 update: change field of client messages -> Messages

--- internal/models/client.go
+++ internal/models/client.go
@@ -4,10 +4,10 @@ import "golang.org/x/net/websocket"
 
 type Client struct {
 	Connection *websocket.Conn
-	messages   chan Message
+	Messages   chan Message
 }
 
 func NewClient(connection *websocket.Conn) *Client {
 	messages := make(chan Message, 1)
-	return &Client{Connection: connection, messages: messages}
+	return &Client{Connection: connection, Messages: messages}
 }
